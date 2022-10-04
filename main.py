3430e256c571dc5fd79a92320c973feb22e76fc1
DiKey10 <kd930603@gmail.com>
2022-10-04 17:21:27 +0200

    dwa

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
-from movie import Movies
+#from movie import Movies
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = Movies().all()
+    movies = tmdb_client.get_popular_movies()["results"][:8]
     return render_template("homepage.html", movies=movies)
+"""
+@app.route('/')
+def homepage():
+    movies = Movies().all()
+    def tmdb_image_url(path):
+        return tmdb_client.get_poster_url(path)
+    return render_template("homepage.html", movies=movies,tmdb_image_url=tmdb_image_url)
+"""
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
 
 
 if __name__ == "__main__":
--- tmdb_client.py
+++ tmdb_client.py
@@ -9,6 +9,9 @@ def get_popular_movies():
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+def get_movies(how_many):
+    data = get_popular_movies()
+    return data["results"][:how_many]
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
